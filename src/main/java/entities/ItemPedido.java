72c6bb17a4518fe42bfaf8db779f70d3e671a798
“Juan Diaz” <juanluisdiazguerrero@gmail.com>
2024-03-25 21:30:51 -0500

    Crea la entidad ItemPedido

---
+++ src/main/java/entities/ItemPedido.java
@@ -0,0 +1,27 @@
+package entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Builder
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column
+    private Integer cantidad;
+
+    @Column
+    private Float precioUnitario;
+
+}
