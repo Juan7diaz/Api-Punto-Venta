1ede5ca5c4682bb8cf15a9a1b8a5f6232bf546f1
DanielJCC <danijocogollo@gmail.com>
2024-04-04 22:41:53 -0500

    test: finalización de los tests de ClienteController y tests de PedidoController hechos

--- src/test/java/org/unimagdalena/tallermicroservicioapi/api/ClienteControllerTest.java
+++ src/test/java/org/unimagdalena/tallermicroservicioapi/api/ClienteControllerTest.java
@@ -7,33 +7,26 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.unimagdalena.tallermicroservicioapi.dto.cliente.ClienteToSaveDto;
 import org.unimagdalena.tallermicroservicioapi.dto.cliente.ClienteToShowDto;
-import org.unimagdalena.tallermicroservicioapi.entities.Cliente;
 import org.unimagdalena.tallermicroservicioapi.services.cliente.ClienteServices;
 
-import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -45,15 +38,13 @@ class ClienteControllerTest {
     private ClienteServices clienteServices;
     @Autowired
     private ObjectMapper objectMapper;
-    private Cliente cliente;
-    private ClienteToShowDto clienteToShowDto;
 
     @BeforeEach
     void setUp() {
     }
 
     @Test
-    void whenGetAllUsuarios_thenReturnListUsuarios() throws Exception {
+    void whenGetAllClientes_thenReturnListClientes() throws Exception {
         ClienteToShowDto cliente1 = new ClienteToShowDto(
                 UUID.randomUUID(),
                 "Jachan Bananaras",
@@ -73,7 +64,7 @@ class ClienteControllerTest {
                 .andExpect(jsonPath("$[0].id",is(cliente1.id().toString())));
     }
     @Test
-    void whenGetUserById_thenReturnsUsuario() throws Exception{
+    void whenGetClienteById_thenReturnsCliente() throws Exception{
         ClienteToShowDto cliente1 = new ClienteToShowDto(
                 UUID.randomUUID(),
                 "Jachan Bananaras",
@@ -81,10 +72,107 @@ class ClienteControllerTest {
                 "Manzana 1 Casa 9 Las Bananeras");
         when(clienteServices.findClienteById(cliente1.id())).thenReturn(cliente1);
 
-        mockMvc.perform(get("/api/v1/customers")
+        mockMvc.perform(get("/api/v1/customers/{id}",cliente1.id())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$",hasSize(2)))
-                .andExpect(jsonPath("$[0].id",is(cliente1.id().toString())));
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.id",is(cliente1.id().toString())));
+    }
+    @Test
+    void whenGetClienteByEmail_thenReturnsCliente() throws Exception{
+        ClienteToShowDto cliente1 = new ClienteToShowDto(
+                UUID.randomUUID(),
+                "Jachan Bananaras",
+                "jachanBananeras@test.com",
+                "Manzana 1 Casa 9 Las Bananeras");
+        when(clienteServices.findClienteByEmail(cliente1.email())).thenReturn(cliente1);
+
+        mockMvc.perform(get("/api/v1/customers/email/{email}",cliente1.email())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.email",is(cliente1.email())));
+    }
+    @Test
+    void whenGetClienteByCity_thenReturnsCliente() throws Exception{
+        ClienteToShowDto cliente1 = new ClienteToShowDto(
+                UUID.randomUUID(),
+                "Jachan Bananaras",
+                "jachanBananeras@test.com",
+                "Manzana 1 Casa 9 Las Bananeras");
+        ClienteToShowDto cliente2 = new ClienteToShowDto(
+                UUID.randomUUID(),
+                "Yan Payaros",
+                "yanPayaros@test.com",
+                "Manzana 2 Casa 92 Las Bananeras");
+        when(clienteServices.findClienteByDireccionContainingIgnoreCase("las bananeras")).thenReturn(List.of(cliente1,cliente2));
+
+        mockMvc.perform(get("/api/v1/customers/city")
+                        .param("cityName","las bananeras")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].direccion",is(cliente1.direccion())))
+                .andExpect(jsonPath("$[1].direccion",is(cliente2.direccion())));
+    }
+    @Test
+    void whenPostCliente_thenReturnsClienteSaved() throws Exception{
+        ClienteToSaveDto clienteToSave = new ClienteToSaveDto(
+                "Jachan Bananaras",
+                "jachanBananeras@test.com",
+                "Manzana 1 Casa 9 Las Bananeras");
+        ClienteToShowDto clienteSaved = new ClienteToShowDto(
+                UUID.randomUUID(),
+                clienteToSave.nombre(),
+                clienteToSave.email(),
+                clienteToSave.direccion());
+
+        when(clienteServices.SaveCliente(clienteToSave)).thenReturn(clienteSaved);
+
+        mockMvc.perform(post("/api/v1/customers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(clienteToSave)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.id",is(clienteSaved.id().toString())))
+                .andExpect(jsonPath("$.nombre",is(clienteSaved.nombre())));
+    }
+    @Test
+    void whenPutClienteById_thenReturnsClienteUpdated() throws Exception{
+        ClienteToSaveDto toUpdate = new ClienteToSaveDto(
+                null,
+                "jachanBananeras@gmail.com",
+                null);
+        ClienteToShowDto clienteUpdated = new ClienteToShowDto(
+                UUID.randomUUID(),
+                "Jachan Bananaras",
+                toUpdate.email(),
+                "Manzana 1 Casa 9 Las Bananeras");
+
+        when(clienteServices.updateClienteById(clienteUpdated.id(),toUpdate)).thenReturn(clienteUpdated);
+
+        mockMvc.perform(put("/api/v1/customers/{id}",clienteUpdated.id())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(toUpdate)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.id",is(clienteUpdated.id().toString())))
+                .andExpect(jsonPath("$.email",is(clienteUpdated.email())));
+    }
+    @Test
+    void whenDeleteClienteById_thenReturnsMessage() throws Exception{
+        ClienteToShowDto cliente1 = new ClienteToShowDto(
+                UUID.randomUUID(),
+                "Jachan Bananaras",
+                "jachanBananeras@test.com",
+                "Manzana 1 Casa 9 Las Bananeras");
+
+       doNothing().when(clienteServices).deleteClienteById(cliente1.id());
+
+        mockMvc.perform(delete("/api/v1/customers/{id}",cliente1.id())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Cliente eliminado"));
     }
 }
---
+++ src/test/java/org/unimagdalena/tallermicroservicioapi/api/PedidoControllerTest.java
@@ -0,0 +1,167 @@
+package org.unimagdalena.tallermicroservicioapi.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.unimagdalena.tallermicroservicioapi.dto.cliente.ClienteToShowDto;
+import org.unimagdalena.tallermicroservicioapi.dto.pedido.PedidoToSaveDto;
+import org.unimagdalena.tallermicroservicioapi.dto.pedido.PedidoToShowDto;
+import org.unimagdalena.tallermicroservicioapi.services.pedido.PedidoServices;
+import org.unimagdalena.tallermicroservicioapi.utils.EstadoPedido;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+class PedidoControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    private PedidoServices pedidoServices;
+    @Autowired
+    private ObjectMapper objectMapper;
+    PedidoToShowDto pedido;
+    ClienteToShowDto cliente;
+    @BeforeEach
+    void setUp() {
+        cliente = new ClienteToShowDto(
+                UUID.randomUUID(),
+                "Jachan Bananaras",
+                "jachanBananeras@test.com",
+                "Manzana 1 Casa 9 Las Bananeras");
+        pedido = new PedidoToShowDto(
+                UUID.randomUUID(),
+                LocalDateTime.of(2024,3,30,10,0),
+                EstadoPedido.PENDIENTE,
+                cliente);
+    }
+
+    @Test
+    void whenGetAllPedidos_thenReturnListPedidos() throws Exception{
+        List<PedidoToShowDto> pedidos = List.of(pedido,new PedidoToShowDto(
+                UUID.randomUUID(),
+                LocalDateTime.of(2024,3,15,12,30),
+                EstadoPedido.ENVIADO,
+                cliente));
+        when(pedidoServices.findAllPedidos()).thenReturn(pedidos);
+
+        mockMvc.perform(get("/api/v1/orders")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(2)))
+                .andExpect(jsonPath("$[0].id",is(pedido.id().toString())));
+    }
+    @Test
+    void whenGetPedidoById_thenReturnsPedido() throws Exception{
+        when(pedidoServices.findPedidoById(pedido.id())).thenReturn(pedido);
+
+        mockMvc.perform(get("/api/v1/orders/{id}",pedido.id())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.id",is(pedido.id().toString())));
+    }
+    @Test
+    void whenGetPedidoByClienteIdAndStatus_thenReturnsPedido() throws Exception{
+        when(pedidoServices.findPedidosByClienteIdAndStatus(cliente.id(),pedido.status())).thenReturn(List.of(pedido));
+
+        mockMvc.perform(get("/api/v1/orders/customer/{customerId}",cliente.id())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("status",pedido.status().toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id",is(pedido.id().toString())));
+    }
+    @Test
+    void whenGetPedidoByDateRange_thenReturnsPedido() throws Exception{
+        LocalDateTime startDate = LocalDateTime.of(2024,3,25,10,0);
+        LocalDateTime endDate = LocalDateTime.of(2024,4,1,17,0);
+        when(pedidoServices.findPedidosByFechaPedidoBetween(startDate,endDate)).thenReturn(List.of(pedido));
+
+        mockMvc.perform(get("/api/v1/orders/date-range")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("startDate",startDate.toString())
+                        .param("endDate",endDate.toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id",is(pedido.id().toString())));
+    }
+    @Test
+    void whenPostPedido_thenReturnsPedidoSaved() throws Exception{
+        PedidoToSaveDto pedidoToSave = new PedidoToSaveDto(
+                pedido.fechaPedido(),
+                pedido.status(),
+                new ClienteToShowDto(
+                        cliente.id(),
+                        null,
+                        null,
+                        null));
+
+        when(pedidoServices.savePedido(pedidoToSave)).thenReturn(pedido);
+
+        mockMvc.perform(post("/api/v1/orders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(pedidoToSave)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.id",is(pedido.id().toString())))
+                .andExpect(jsonPath("$.status",is(pedido.status().toString())));
+    }
+    @Test
+    void whenPutPedidoById_thenReturnsPedidoUpdated() throws Exception{
+        PedidoToSaveDto toUpdate = new PedidoToSaveDto(
+                null,
+                EstadoPedido.ENTREGADO,
+                new ClienteToShowDto(
+                        cliente.id(),
+                        null,
+                        null,
+                        null));
+        PedidoToShowDto pedidoUpdated = new PedidoToShowDto(
+                pedido.id(),
+                pedido.fechaPedido(),
+                toUpdate.status(),
+                pedido.cliente());
+
+        when(pedidoServices.updatePedidoById(pedidoUpdated.id(),toUpdate)).thenReturn(pedidoUpdated);
+
+        mockMvc.perform(put("/api/v1/orders/{id}",pedidoUpdated.id())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(toUpdate)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.id",is(pedidoUpdated.id().toString())))
+                .andExpect(jsonPath("$.status",is(pedidoUpdated.status().toString())));
+    }
+    @Test
+    void whenDeletePedidoById_thenReturnsMessage() throws Exception{
+        doNothing().when(pedidoServices).deletePedidoById(pedido.id());
+
+        mockMvc.perform(delete("/api/v1/orders/{id}",pedido.id())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Pedido Eliminado"));
+    }
+}
